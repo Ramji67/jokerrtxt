efa3b998302d1654ec0df7e74383b49e717a8a19
Nickle12 <166562244+Nickle12@users.noreply.github.com>
2024-04-22 14:04:44 +0530

    Update vars.py

--- Ankush/vars.py
+++ Ankush/vars.py
@@ -1,5 +1,5 @@
-api_id = 20491966
-api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
-bot_token = "6615366987:AAFqURUR5DibftknVctiPgJ_7ZOIjwthmL4"
+api_id = 28498858
+api_hash = "6cc3cfdeae65b6ae7f47fb828edf38f4"
+bot_token = "7070667789:AAE-W5tvm9x-E4FVss4MmSG03ca3ktnRbbU"
 AUTH = 5881684718
 #Bot Username = "Anajajbsbsbsbot"
