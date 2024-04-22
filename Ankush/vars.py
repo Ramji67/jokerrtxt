b8d51045d842ac57437886b5197a90ae09b62ad4
Nickle12 <166562244+Nickle12@users.noreply.github.com>
2024-04-22 22:36:03 +0530

    Update vars.py

--- Ankush/vars.py
+++ Ankush/vars.py
@@ -1,5 +1,32 @@
-api_id = 28498858
-api_hash = "6cc3cfdeae65b6ae7f47fb828edf38f4"
-bot_token = "7070667789:AAE-W5tvm9x-E4FVss4MmSG03ca3ktnRbbU"
-AUTH = 5881684718
-#Bot Username = "Anajajbsbsbsbot"
+import os
+
+
+
+api_id = int(os.environ.get("API_ID"))
+
+api_hash = os.environ.get("API_HASH")
+
+bot_token = os.environ.get("BOT_TOKEN")
+
+
+
+
+
+OWNER = int(os.environ.get("OWNER", 841021123))
+
+
+
+try:
+
+    ADMINS=[]
+
+    for x in (os.environ.get("ADMINS", "841021123").split()):
+
+        ADMINS.append(int(x))
+
+except ValueError:
+
+        raise Exception("Your Admins list does not contain valid integers.")
+
+ADMINS.append(OWNER)
+
